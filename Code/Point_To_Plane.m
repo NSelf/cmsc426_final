5b6aa808b7f8c05e61e0d96010718b20c664935b
spattana123 <spattana@umd.edu>
2018-05-10 23:51:05 -0400

    Updated Changed version

--- Code/Point_To_Plane.m
+++ Code/Point_To_Plane.m
@@ -1,6 +1,6 @@
 function [R, T] = Point_To_Plane(p1, p2, norm)
 
-    Normals = norm;
+    Nmls = norm;
     if(size(p1,1) ~= 3 & size(p2,1) ~= 3)
         m = min(size(p1,1),size(p2,1));
         if(size(p1,1) < size(p2,1))
@@ -8,42 +8,61 @@ function [R, T] = Point_To_Plane(p1, p2, norm)
         else
             p1 = p1(1:m,:);
         end
-        Normals = Normals(1:m,:);
-        Normals = Normals'
+        Nmls = Nmls(1:m,:);
+        Nmls = Nmls'
         p1 = p1';
         p2 = p2';
     else
-        Normals = Normals';
+        Nmls = Nmls';
     end
-    Diff = p1-p2; 
-    b = transpose(sum(Diff.*Normals,1)); 
-    A1 = Normals(3,:).*p2(2,:)-Normals(2,:).*p2(3,:);
-    A2 = Normals(1,:).*p2(3,:)-Normals(3,:).*p2(1,:);
-    A3 = Normals(2,:).*p2(1,:)-Normals(1,:).*p2(2,:);
-    A = [A1;A2;A3;Normals]'; 
+    sub = p1-p2; 
+    b = sum(sub.*Nmls,1)';
+    
+    A11 = Nmls(3,:).*p2(2,:);
+    A12 = Nmls(2,:).*p2(3,:);
+    A1 = A11 - A12;
+    
+    A21 = Nmls(1,:).*p2(3,:);
+    A22 = Nmls(3,:).*p2(1,:);
+    A2 = A21-A22;
+    
+    A31 = Nmls(2,:).*p2(1,:);
+    A32 = Nmls(1,:).*p2(2,:);
+    A3 = A31 - A32;
+    A = [A1;A2;A3;Nmls]'; 
     
     x = A\b;
     
-    cosa = cos(x(1));
-    sina = sin(x(1));
-    cosb = cos(x(2));
-    sinb = sin(x(2));
-    cosg = cos(x(3));
-    sing = sin(x(3));
+    one = x(1);
+    two = x(2);
+    three = x(3);
     
-    a = cosg*cosb;
-    b = -sing*cosa+cosg*sinb*sina;
-    c = sing*sina+cosg*sinb*cosa;
-    d = sing*cosb;
-    e = cosg*cosa+sing*sinb*sina;
-    f = -cosg*sina+sing*sinb*cosa;
-    g = -sinb;
-    h = cosb*sina
-    i = cosb*cosa;
+    aca = cos(one);
+    asa = sin(one);
+    bcb = cos(two);
+    bsb = sin(two);
+    gcg = cos(three);
+    gsg = sin(three);
     
-    R = [a, b, c;
-         d, e, f;
-         g, h, i];
+    a = gcg*bcb;
+    b1 = -gsg*aca;
+    b2 = gcg*bsb*asa;;
+    b = b1+b2;
+    c1 = gsg*asa; 
+    c2 = gcg*bsb*aca;
+    c = c1+c2;
+    d = gsg*bcb;
+    e1 = gcg*aca;
+    e2 = gsg*bsb*asa;
+    e = e1+e2;
+    f1 = -gcg*asa;
+    f2 = gsg*bsb*aca;
+    f = f1+f2;
+    g = -bsb;
+    h = bcb*asa
+    i = bcb*aca;
+    
+    R = [a, b, c; d, e, f; g, h, i];
     T = x(4:end);
     return;
 end
